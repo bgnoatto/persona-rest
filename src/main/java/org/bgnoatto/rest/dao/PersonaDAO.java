fb9e823588397b054344578ed76ed95f4ab862ff
Bruno Gnoatto <bgnoatto@gmail.com>
2019-03-06 12:47:46 -0300

    minor fix

--- src/main/java/org/bgnoatto/rest/dao/PersonaDAO.java
+++ src/main/java/org/bgnoatto/rest/dao/PersonaDAO.java
@@ -19,26 +19,12 @@ public class PersonaDAO {
 		if(criteria.list()!=null || !criteria.list().isEmpty()) {
 			List<Persona> results = criteria.list();
 			Iterator<Persona> it = results.iterator();
-			
 			while(it.hasNext()) {
 				resultado.add(it.next());
 			}
 		}
 		
-		
 		session.getTransaction().commit();
-//		HibernateUtil.shutdown();
-//		try {
-//			conn = getDBConnection();
-//			Statement statement = conn.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE,
-//					ResultSet.CONCUR_READ_ONLY);
-//			ResultSet result = statement.executeQuery("SELECT dni,nombre,apellido,edad FROM persona");
-//			System.out.println(result);
-//		} catch (Exception e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
-		
 		return resultado;
 	}
 	
@@ -47,84 +33,6 @@ public class PersonaDAO {
 		session.beginTransaction();
 		session.saveOrUpdate(persona);
 		session.getTransaction().commit();
-//		HibernateUtil.shutdown();
 		return persona;
 	}
-
-//	static private PersonaDAO personaDAO = null;
-//
-//	static private Connection conn = null;
-//
-//	static private String DRIVER = "org.hsqldb.jdbc.JDBCDriver";
-//	static private String URL = "jdbc:hsqldb:mem:.";
-//	static private String USER = "SA";
-//	static private String PASSWD = "";
-
-//	public PersonaDAO() {
-//		super();
-//	}
-
-//	static public PersonaDAO getInstance() throws Exception {
-//		if (personaDAO == null) {
-//			personaDAO = new PersonaDAO();
-//
-//			conn = personaDAO.getDBConnection();
-//		}
-//
-//		return personaDAO;
-//	}
-
-//	protected Connection getDBConnection() {
-//
-//		try {
-//			if (conn != null && !conn.isClosed()) {
-//				return conn;
-//			}
-//			
-//			// Registering the HSQLDB JDBC driver
-//			Class.forName("org.hsqldb.jdbc.JDBCDriver");
-//			
-//			conn = initDatabase();
-//
-//		} catch (Exception e1) {
-//			// TODO Auto-generated catch block
-//			e1.printStackTrace();
-//		}
-//
-//		
-//		return conn;
-//	}
-	
-	/**
-	 * Database initialization for testing i.e.
-	 * <ul>
-	 * <li>Creating Table</li>
-	 * <li>Inserting record</li>
-	 * </ul>
-	 * 
-	 * @throws SQLException
-	 */
-//	private static Connection initDatabase() {
-//		try {
-//			conn = getConnection(); Statement statement = conn.createStatement();
-//			statement.execute("CREATE TABLE IF NOT EXISTS persona (dni INT NOT NULL, nombre VARCHAR(250),"
-//					+ "apellido VARCHAR(250), edad INT, PRIMARY KEY (dni))");
-//			conn.commit();
-//		}catch (SQLException e) {
-//			conn = null;
-//			e.printStackTrace();
-//			
-//		}
-//		return conn;
-//	}
-	
-	/**
-	 * Create a connection
-	 * 
-	 * @return connection object
-	 * @throws SQLException
-	 */
-//	private static Connection getConnection() throws SQLException {
-//		return DriverManager.getConnection("jdbc:hsqldb:mem:personas", "SA", "");
-//	}
 }
--- src/main/java/org/bgnoatto/rest/domain/Persona.java
+++ src/main/java/org/bgnoatto/rest/domain/Persona.java
@@ -31,7 +31,6 @@ public class Persona implements Serializable{
 	
 	public Persona() {
 		super();
-		// TODO Auto-generated constructor stub
 	}
 	public Persona(Integer dni, String nombre, String apellido, Integer edad) {
 		super();
@@ -72,6 +71,47 @@ public class Persona implements Serializable{
 		this.edad = edad;
 	}
 	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((apellido == null) ? 0 : apellido.hashCode());
+		result = prime * result + ((dni == null) ? 0 : dni.hashCode());
+		result = prime * result + ((edad == null) ? 0 : edad.hashCode());
+		result = prime * result + ((nombre == null) ? 0 : nombre.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Persona other = (Persona) obj;
+		if (apellido == null) {
+			if (other.apellido != null)
+				return false;
+		} else if (!apellido.equals(other.apellido))
+			return false;
+		if (dni == null) {
+			if (other.dni != null)
+				return false;
+		} else if (!dni.equals(other.dni))
+			return false;
+		if (edad == null) {
+			if (other.edad != null)
+				return false;
+		} else if (!edad.equals(other.edad))
+			return false;
+		if (nombre == null) {
+			if (other.nombre != null)
+				return false;
+		} else if (!nombre.equals(other.nombre))
+			return false;
+		return true;
+	}
 	@Override
 	public String toString() {
 		return "Persona [dni=" + dni + ", nombre=" + nombre + ", apellido=" + apellido + ", edad=" + edad + "]";
